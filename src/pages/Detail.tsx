074d467991f6d7a5f3dde794eacc567d90ef2447
Jorge Rangel Jimenez <georgerangelcode@gmail.com>
2022-04-24 18:51:12 -0500

    add seo

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet";
 import Breadcrumb from "../components/Breadcrumb";
 import useGetProduct from "../hooks/useGetProduct";
 import useStore from "../store";
@@ -8,34 +9,49 @@ const Detail = () => {
   useGetProduct();
 
   return (
-    <main className="page--container">
-      <div>
-        <Breadcrumb />
-        {item && (
-          <div className="detail-container">
-            <div className="detail-first-row">
-              <div className="detail-img-container">
-                <img src={item.picture} alt={item.title} />
+    <>
+      <Helmet>
+        <title>{item?.title}</title>
+        <meta name="twitter:title" content={item?.title} />
+        <meta name="twitter:description" content={item?.description} />
+        <meta name="twitter:image" content={item?.picture} />
+        <meta property="og:title" content={item?.title} />
+        <meta property="og:description" content={item?.description} />
+        <meta property="og:image" content={item?.picture} />
+        <meta property="og:url" content="https://meli-sable.vercel.app/" />
+        <meta property="og:site_name" content="Meli" />
+        <meta property="og:locale" content="es_ES" />
+        <meta property="og:type" content="article" />
+      </Helmet>
+      <main className="page--container">
+        <div>
+          <Breadcrumb />
+          {item && (
+            <div className="detail-container">
+              <div className="detail-first-row">
+                <div className="detail-img-container">
+                  <img src={item.picture} alt={item.title} />
+                </div>
+                <div className="detail-info">
+                  <p className="detail-condition-sold">
+                    {`${item.condition === "new" ? "Nuevo" : "Usado"} - ${item.sold_quantity} vendidos`}
+                  </p>
+                  <h5 className="detail-title">{item.title}</h5>
+                  <h3 className="detail-price">$ {Intl.NumberFormat("de-DE").format(item.price.amount)}</h3>
+                  <button aria-label="Comprar Articulo" className="detail-buy">
+                    Comprar
+                  </button>
+                </div>
               </div>
-              <div className="detail-info">
-                <p className="detail-condition-sold">
-                  {`${item.condition === "new" ? "Nuevo" : "Usado"} - ${item.sold_quantity} vendidos`}
-                </p>
-                <h5 className="detail-title">{item.title}</h5>
-                <h3 className="detail-price">$ {Intl.NumberFormat("de-DE").format(item.price.amount)}</h3>
-                <button aria-label="Comprar Articulo" className="detail-buy">
-                  Comprar
-                </button>
+              <div className="detail-description">
+                <p className="detail-description-title">Descripción del producto</p>
+                <p className="detail-description-text">{item.description}</p>
               </div>
             </div>
-            <div className="detail-description">
-              <p className="detail-description-title">Descripción del producto</p>
-              <p className="detail-description-text">{item.description}</p>
-            </div>
-          </div>
-        )}
-      </div>
-    </main>
+          )}
+        </div>
+      </main>
+    </>
   );
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,24 @@
+import { Helmet } from "react-helmet";
 import ListProducts from "../components/ListProducts";
 import useStore from "../store";
 
 const Home = () => {
   const showSpinner = useStore(state => state.showSpinner);
 
-  return <main className="page--container">{!showSpinner && <ListProducts />}</main>;
+  return (
+    <>
+      <Helmet>
+        <title>Meli - Articulos</title>
+        <meta name="twitter:title" content="Meli" />
+        <meta name="twitter:description" content="Meli - Articulos" />
+        <meta property="og:title" content="Meli" />
+        <meta property="og:description" content="Meli - Articulos" />
+        <meta property="og:site_name" content="Meli" />
+        <meta property="og:locale" content="es_ES" />
+      </Helmet>
+      <main className="page--container">{!showSpinner && <ListProducts />}</main>
+    </>
+  );
 };
 
 export default Home;
