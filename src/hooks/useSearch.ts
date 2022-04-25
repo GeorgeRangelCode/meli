a31307a7627b327ff427d7955156cd44fff836fa
Jorge Rangel Jimenez <georgerangelcode@gmail.com>
2022-04-25 15:00:29 -0500

    add search mount component

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -1,11 +1,19 @@
-import { useState } from "react";
-import { createSearchParams, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { createSearchParams, useNavigate, useSearchParams } from "react-router-dom";
 import useStore from "../store";
 
 const useSearch = () => {
   const navigate = useNavigate();
   const [valueSearch, setValueSearch] = useState("");
   const onSubmit = useStore(state => state.onSubmit);
+  const [searchParams] = useSearchParams();
+  const valueSearchParams = searchParams.get("search");
+
+  useEffect(() => {
+    if (valueSearchParams) {
+      onSubmit(valueSearchParams);
+    }
+  }, [onSubmit, valueSearchParams]);
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
